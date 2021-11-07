b22a2dafa378a186fd13d8e1fc1a9e0e5771cd7b
Falendrac <leger_franck@hotmail.fr>
2021-11-07 20:38:07 +0000

    Add _printf.c (Work in progress)

---
+++ _printf.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _printf - This is our printf function
+ * That print all format is possible
+ *
+ * @format: All arguments we want to print in the
+ * same format
+ *
+ * Return: Upon sucessful return, the functions return the number of
+ * charaters printed.
+ */
+int _printf(const char *format, ...)
+{
+
+	return (0);
+}
