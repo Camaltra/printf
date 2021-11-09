0f1e6662b97b8fe954bd387c265620e775f610df
Camaltra <camaltra38@gmail.com>
2021-11-09 15:18:50 +0000

    Modification of print_int

--- print_int.c
+++ print_int.c
@@ -7,26 +7,8 @@
  *
  * Return: Lenght of int
  */
-/*int print_number(va_list arg)
+int print_int(va_list arg)
 {
-	unsigned int number;
-	int n = va_arg(arg, int);
-	int i;
+	int number = va_arg(arg, int);
 
-	if (n >= 0)
-	{
-		number = n;
-	}
-	else
-	{
-		_putchar(45);
-		number = -n;
-	}
-	if (number / 10)
-	{
-		print_number(number / 10);
-	}
-	_putchar(number % 10 + '0');
 }
-
-*/
